b8ce6c732f47eb496c21c01c5b1ebcada65d9fee
badal <mishrabadal988@gmail.com>
2024-05-09 18:08:10 +0530

    day-9 task complete task upto branding menu complete and data stores in databse

--- employee_pannel/index.php
+++ employee_pannel/index.php
@@ -15,7 +15,7 @@
     <script src="../common_files/js/bootstrap.bundle.min.js"></script>
     <script src="../common_files/js/popper.min.js"></script>
     <link rel="stylesheet" href="css/index.css">
-    <script src="js/index.js"></script>
+    <script src="js/iindex.js"></script>
 
 
 </head>
@@ -23,6 +23,12 @@
 <body>
     <div class="container-fluid">
         <div class="sidebar">
+        <button class="btn  w-100 text-left stock-update-btn w-100">
+                <i class="fa fa-image"></i>
+               Branding details
+                <i class="fa fa-angle-down close mt-2 "></i>
+            </button>
+
             <button class="btn btn-danger w-100 text-left stock-update-btn w-100">
                 <i class="fa fa-shopping-cart"></i>
                 Stock update
@@ -36,12 +42,131 @@
         </div>
         <div class="page">
     
+        <div class="row">
+<div class="col-md-2"></div>
+<div class="col-md-8 p-4 bg-white rounded-1g shadow-sm">
+    
 
+
+    <form class="branding-form">
+    <div class="form-group ">
+<label for="phone" class="font-weight-bold">enter brand name</label>
+<input type="text" class="form-control w-100" name="brand-name" id="phone" placeholder="shopwap"> </div>
+
+<div class="form-group ">
+<label for="phone" class="font-weight-bold">Upload brand logo</label>
+<input type="file" accept="image/*"  class="form-control w-100" name="brand-logo" id="brand-logo" > </div>
+
+<div class="form-group ">
+<label for="phone" class="font-weight-bold">enter domain name</label>
+<input type="text" class="form-control w-100" name="domain-name" id="phone"  placeholder="www.shopwap.com" > </div>
+
+<div class="form-group ">
+<label for="phone" class="font-weight-bold">email</label>
+<input type="text" class="form-control w-100" name="email" id="phone" placeholder="shopwap@gmail.com"> </div>
+<div class="form-group ">
+<label for="phone" class="font-weight-bold">social handle</label>
+<input type="text" class="form-control w-100" name="facebook-url" id="phone" placeholder="facebook page url"> <input type="text" class="form-control w-100" name="twitter-url" id="phone" placeholder="twitter page url"></div>
+
+<div class="form-group">
+<label for="about-us" class="font-weight-bold">Addresss</label>
+<textarea class="form-control"  name="address"></textarea>
+</div>
+
+
+<div class="form-group ">
+<label for="phone" class="font-weight-bold">Phone</label>
+<input type="text" class="form-control w-100" name="phone" id="phone" placeholder="1800 1200 4005"> </div>
+<div class="form-group">
+<label for="about-us" class="font-weight-bold">About us <small class="about-us-count"> 0 </small><small>/ 5000</small></label>
+<textarea class="form-control"  name="about-us" maxlength="5000" id="about-us"></textarea>
+</div>
+<div class="form-group">
+<label for="about-us" class="font-weight-bold">privacy policy <small class="privacy-count"> 0 </small><small>/ 5000</small> </label>
+<textarea class="form-control"  name="privacy-policy" id="privacy-policy" maxlength="5000"></textarea>
+</div>
+<div class="form-group">
+<label for="about-us" class="font-weight-bold">cookies policy <small class="cookies-count"> 0 </small><small>/ 5000</small> </label>
+<textarea class="form-control"  name="cookies-policy" id="cookies-policy" maxlength="5000"></textarea>
+</div>
+
+<div class="form-group">
+<label for="about-us" class="font-weight-bold">terms and condition </label>
+<textarea class="form-control"  name="terms" id="cookies-policy" maxlength="5000"></textarea>
+</div>
+
+<button type="submit" class="branding-submit-btn btn btn-primary py-2">Submit your information</button>
+
+</form>
+
+
+
+
+</div>
+<div class="col-md-2"></div>
+</div>
+</div>
 
 
 
         </div>
     </div>
 </body>
+<!-- textarea length count -->
+<script>
+$(document).ready(function(){
+$("#about-us").on("input", function(){ 
+    var length = $(this).val().length;
+ $(".about-us-count").html(length); 
 
+});
+});
+
+
+$(document).ready(function(){
+$("#privacy-policy").on("input", function(){
+var length = $(this).val().length;
+$(".privacy-count").html(length);
+});
+});
+
+
+$(document).ready(function(){
+$("#cookies-policy").on("input", function(){
+     var length = $(this).val().length;
+      $(".cookies-count").html(length); 
+
+});
+});
+
+
+// point solution 
+// branding detail
+$(document).ready(function(){
+$(".branding-form").submit(function(e){
+e.preventDefault();
+
+
+var file = document.querySelector("#brand-logo"); 
+var file_size = file.files[0].size; 
+if(200000>file_size){
+$.ajax({
+type: "POST",
+url: "php/branding.php",
+data: new FormData(this),
+processData: false,
+contentType: false,
+cache: false,
+success: function(response)
+{
+document.write(response);
+}
+});
+}
+else{
+    alert("upload pic less than 200kb");
+}
+});
+});
+</script>
 </html>
---
+++ employee_pannel/php/branding.php
@@ -0,0 +1,78 @@
+<?php
+require_once("../../common_files/databases/databases.php");
+$file = $_FILES['brand-logo'];
+$location = $file['tmp_name'];
+// $string = base64_encode(file_get_contents($location));
+// $complete_src = "data:image/png;base64,".$string;
+//  echo "<img src='".$complete_src."'>";
+
+$file = $_FILES['brand-logo'];
+$location = $file['tmp_name'];
+$logo = addslashes(file_get_contents($location));
+$brand_name = $_POST['brand-name'];
+$domain_name = $_POST['domain-name'];
+$email = $_POST['email'];
+$facebook_url = $_POST['facebook-url'];
+$twitter_url = $_POST['twitter-url'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+$about_us = addslashes($_POST['about-us']);
+$privacy_policy = addslashes($_POST['privacy-policy']);
+$cookies_policy = addslashes($_POST['cookies-policy']);
+$terms = addslashes($_POST['terms']);
+
+$check_branding_table = "SELECT * FROM branding";
+$response = $db->query($check_branding_table);
+if ($response) {
+    $store_data = "INSERT INTO branding (brand_name, brand_logo, email, domain_name, facebook_url, twitter_url, address, phone, about_us, privacy_policy, cookies_policy, terms_policy) VALUES ('$brand_name', '$logo' ,'$email','$domain_name', '$facebook_url', '$twitter_url', '$ address', '$phone', '$about_us', '$privacy_policy', '$
+    cookies_policy', '$terms')";
+ $response = $db->query($store_data);
+if($response)
+{
+echo "success";
+}
+else{
+    echo "data not stored";
+    echo $db->error;
+}
+}
+
+else{
+    $create_table = "CREATE TABLE branding( 
+id INT(11) NOT NULL AUTO_INCREMENT,
+brand_name VARCHAR(50),
+brand_logo MEDIUMBLOB,
+ domain_name VARCHAR(100), 
+email VARCHAR(100),
+ facebook_url VARCHAR(255),
+twitter_url VARCHAR(255), 
+address VARCHAR(100),
+phone INT(12), about_us MEDIUMTEXT, 
+ privacy_policy MEDIUMTEXT,
+ cookies_policy MEDIUMTEXT, 
+terms_policy MEDIUMTEXT, 
+    PRIMARY KEY(id)
+)";
+$response = $db->query($create_table);
+//echo "table created";
+$response = $db->query($create_table);
+if($response)
+{
+$store_data = "INSERT INTO branding (brand_name, brand_logo, email, domain_name, facebook_url, twitter_url, address, phone, about_us, privacy_policy, cookies_policy, terms_policy) VALUES ('$brand_name', '$logo' ,'$email','$domain_name', '$facebook_url', '$twitter_url', '$ address', '$phone', '$about_us', '$privacy_policy', '$
+cookies_policy', '$terms')";
+ $response = $db->query($store_data);
+if($response)
+{
+echo "success";
+}
+else{
+    echo "data not stored";
+}
+}
+else{
+    
+echo "unable to create table ";
+    
+}
+}
+?>
